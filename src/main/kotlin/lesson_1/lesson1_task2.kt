22aec58cccbfda2126a4cbecae1ee89da1b7293d
susli <suslikkrol@gmail.com>
2024-02-17 17:29:57 +0400

    KS-3-1 hello bank

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -3,10 +3,12 @@ package org.example.lesson_1
 fun main() {
     val numberOfOrder: Int = 75
     val thanksForPurchase: String = "Большое спасибо за заказ! Мы очень рады забрать Ваши деньги :)"
+
     println(numberOfOrder)
     println(thanksForPurchase)
 
     var amountOfWorkers: Int = 2000
+
 //    println(amountOfWorkers)
 
     amountOfWorkers = 1999
