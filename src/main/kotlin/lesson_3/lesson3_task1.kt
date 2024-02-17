b7858be6988bd75c4aeeb7cdf8285cfac58d93e2
susli <suslikkrol@gmail.com>
2024-02-17 08:08:32 +0400

    KS-3-1 hello bank

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_3
+
+fun main() {
+    val userName: String = "Доктор Хайзенберг"
+    var helloBank: String = "Добрый день"
+    println("$helloBank, $userName!")
+
+    helloBank = "Добрый вечер"
+    println("$helloBank, $userName!")
+}
