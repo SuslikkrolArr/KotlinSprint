c60a8fa62db836f47d3fded4f684fbdaa82e1fcb
susli <suslikkrol@gmail.com>
2024-02-18 13:44:34 +0400

    KS-3-1 hello bank

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,9 +3,11 @@ package org.example.lesson_2
 fun main() {
     val crystalOre: Int = 7
     val ironOre: Int = 11
+    val percentageOFCrystalOreNoBuff: Int = 100
+    val percentageIronOreNoBuff: Int = 100
 
-    val buffCrystalOre: Int = (crystalOre * buff20) / 100
-    val buffIronOre: Int = (ironOre * buff20) / 100
+    val buffCrystalOre: Int = (crystalOre * buff20) / percentageOFCrystalOreNoBuff
+    val buffIronOre: Int = (ironOre * buff20) / percentageIronOreNoBuff
 
     println("Было добыто $buffCrystalOre шт. бонусной кристаллической руды")
     println("Было добыто $buffIronOre шт. бонусной железной руды")
