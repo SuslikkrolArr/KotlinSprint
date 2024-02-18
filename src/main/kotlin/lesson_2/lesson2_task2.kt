61178e8ea76f6d475a1dc3935e618bcc9c1d214e
susli <suslikkrol@gmail.com>
2024-02-18 05:27:58 +0400

    KS-3-1 hello bank

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -3,14 +3,15 @@ package org.example.lesson_2
 fun main() {
     val nemberOfEmployees: Int = 50
     val namberOfTrainees: Int = 30
+    val salaryEmployees: Int = 30000
+    val salaryTrainees: Int = 20000
 
-    val employeeSalaryExpenses: Int = nemberOfEmployees * 30000
-    val generalExpenses: Int = (nemberOfEmployees * 30000) + (namberOfTrainees * 20000)
+    val employeeSalaryExpenses: Int = nemberOfEmployees * salaryEmployees
+    val generalExpenses: Int = (nemberOfEmployees * salaryEmployees) + (namberOfTrainees * salaryTrainees)
     val averageSalary: Int = generalExpenses / (nemberOfEmployees + namberOfTrainees)
 
     println("Расходы на выплату ЗП постоянным сторудникам: $employeeSalaryExpenses")
     println("Общие расходы по ЗП: $generalExpenses")
     println("Средняя зарплата одного сотрудника: $averageSalary")
-
 
 }
