9c7e142c0358fdbae07a2af968116a352a8509f2
susli <suslikkrol@gmail.com>
2024-02-19 06:15:36 +0400

    KS-3-3 multiplication table

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    var startOfTheMove: String = "E2"
+    var endOfStroke: String = "E4"
+    var moveNumber: Int = 1
+
+    println("$startOfTheMove - $endOfStroke; $moveNumber")
+
+    startOfTheMove = "D2"
+    endOfStroke = "D3"
+    moveNumber = 2
+
+    println("$startOfTheMove - $endOfStroke; $moveNumber")
+}
